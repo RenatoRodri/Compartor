856220096938aa7aa643608085a83e2f331f5e3d
Renato Rodrigues <renato282930@gmail.com>
2020-04-21 16:11:26 -0300

    Comparator -2

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,7 +21,9 @@ public class Program {
 		list.add(new Product("Smart Tv", 4000.00));
 		list.add(new Product("Telefone", 1500.00));
 		
-		list.sort(new Mycompable());
+		Comparator <Product> comp = (p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		
+		list.sort(comp);
 		
 		
 		for(Product p: list) {
